7eae329c8095289ab58f0fd1a65bb18ebc1c9ee1
andrewske <andrewskevin92@gmail.com>
2020-06-15 18:33:05 +0800

    Remove login from landing page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -7,13 +7,11 @@ const Landing = () => {
       <div className='dark-overlay'>
         <div className='landing-inner'>
           <h1 className='x-large'>Music Minion</h1>
+          <h3>Coming Soon...</h3>
           <p className='lead'>
             Organize your Music Library with Tags. Discover New Music. Explore
             your library through data visualizations.
           </p>
-          <div className='buttons'>
-            <SpotifyLoginButton />
-          </div>
         </div>
       </div>
     </section>
